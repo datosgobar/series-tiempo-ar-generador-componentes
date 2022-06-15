1738ab6a82fa22938c0c551efcf8b3806c3c80cf
jpinero <jpinero@fusap.com.ar>
2022-06-15 18:59:04 -0300

    refactor . migro algunas funciones a utils.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,15 @@ import axios from 'axios';
 import {AxiosResponse} from 'axios';
 import queryString, { ParsedQuery } from 'query-string';
 import {defaultOptions as highChartDefaultOptions} from 'highcharts';
+import {
+    clearCard,
+    clearGraph,
+    filterAllFalsyValues,
+    generateCardHTML,
+    generateChartTypeSelects,
+    generateGraphHTML, updateCardErrorContainer,
+    updateGraphErrorContainer
+} from "./utils";
 const windowObject = window as any;
 const TSComponents = windowObject.TSComponents;
 const API_SERIES_URL:string = "https://apis.datos.gob.ar/series/api/series/";
@@ -63,8 +72,7 @@ let defaultGraphParameters:GraphicParameters = {
     zoom: false
 
 }
-let outputCardParameters:CardParameters ;
-let outputGraphParameters:GraphicParameters;
+
 let counterCard:number = 1;
 let counterGraph:number = 1;
 
@@ -214,150 +222,9 @@ function updateValuesGraph () {
 
 
 }
-function HTMLRowsForNotDefaultCardParameters(){
-    outputCardParameters = Object.assign({},context.cardParameters); //arranco con los values actuales
-    const mapDefault = new Map (Object.entries({...defaultCardParameters,numbersAbbreviate:true}));//  el valor por default de numbAbb es true, pero esta seteado en false arriba por practicidad (ver comentario arriba)
-    let mapOutput = new Map (Object.entries(outputCardParameters));
-    let htmlOutputForNotDefaultProps:string=getHtmlForDiffFields(mapOutput,mapDefault);
 
-    return htmlOutputForNotDefaultProps;
-  }
 
-function getHtmlForDiffFields(mapOutput: Map<any, any> , mapDefault: Map<any,any>) {
-    let htmlOutputForNotDefaultProps: string = ""
-    mapOutput.forEach((value, key, map) => {
-        if (mapDefault.get(key) == value) {
-            map.delete(key);
-        }
-    })
-    console.log(mapOutput);
-    mapOutput.forEach((value, key) => {
-        let separatorBegin = (value != true && value != false) ? "'" : " ";
-        let separatorEnd = separatorBegin;
 
-        if(key.toString().includes('chartOptions')){
-            value = JSON.stringify(value);
-            separatorEnd='';
-            separatorBegin='';
-        }
-        if(key.toString().includes('colors')) {
-            let formattedArray:Array<string|number> = value as Array<string|number>;
-            let realformat = formattedArray.map((x,index)=>{
-                if(index%2){
-                   return "\""+x+"\"";
-                }else{
-                    return x;
-                }
-            })
-            separatorBegin = '[';
-            separatorEnd = ']';
-             value = realformat;
-        }
-        htmlOutputForNotDefaultProps += "<span class='nx'>" + key + "</span><span class='o'>:</span>" +
-            "<span class='s1'>" + separatorBegin + value.toString() + separatorEnd + ",</span>\n            ";
-    })
-    return htmlOutputForNotDefaultProps;
-}
-
-function HTMLRowsForNotDefaultGraphParameters(){
-    outputGraphParameters = Object.assign({},context.graphicParameters); //arranco con los values actuales
-    const mapDefault = new Map (Object.entries({...defaultGraphParameters}));//  el valor por default de numbAbb es true, pero esta seteado en false arriba por practicidad (ver comentario arriba)
-    let mapOutput = new Map (Object.entries(outputGraphParameters));
-    let htmlOutputForNotDefaultProps = getHtmlForDiffFields(mapOutput, mapDefault);
-    return htmlOutputForNotDefaultProps;
-}
- function filterAllFalsyValues(obj:any){
-
-   return  Object.entries(obj).reduce(
-       (a:any,[k,v]) => (!(v!==false&&(v==undefined||v=="")) ? (a[k]=v, a) : a)
-       , {});
-}
-function generateCardHTML() {
-    let html:string ="<pre>\n" +
-        "<span class='c'>&lt;!-- código HTML donde ubicar un div con una tarjeta --&gt;</span>\n" +
-        "<span class='p'>&lt;</span>" +
-        "<span class='nt'>div</span> " +
-        "<span class='na'>id</span>" +
-        "<span class='o'>=</span>" +
-        "<span class='s'>'tmi'</span>" +
-        "<span class='p'>&gt;&lt;/</span> " +
-        "<span class='nt'>div</span>" +
-        "<span class='p'>&gt;</span>\n\n" +
-        "<span class='c'>&lt;!-- JS que genera la tarjeta en el div --&gt;</span>\n" +
-        "<span class='p'>&lt;</span>" +
-        "<span class='nt'>script</span>" +
-        "<span class='p'>&gt;</span>\n    " +
-        "<span class='nb'>window</span>" +
-        "<span class='p'>.</span>" +
-        "<span class='nx'>onload</span> " +
-        "<span class='o'>=</span> " +
-        "<span class='kd'>function</span>" +
-        "<span class='p'>()</span> " +
-        "<span class='p'>{</span>\n        " +
-        "<span class='nx'>TSComponents</span><span class='p'>.</span>" +
-        "<span class='nx'>Card</span>" +
-        "<span class='p'>.</span>" +
-        "<span class='nx'>render</span>" +
-        "<span class='p'>(</span>" +
-        "<span class='s1'>'tmi'</span>" +
-        "<span class='p'>,</span> "+
-        "<span class='p'>{</span>\n            " +
-        HTMLRowsForNotDefaultCardParameters() +
-        "<span class='p'>})</span>\n    " +
-        "<span class='p'>}</span>\n" +
-        "<span class='p'>&lt;/</span>" +
-        "<span class='nt'>script</span>" +
-        "<span class='p'>&gt;</span>\n" +
-        "</pre>"
-
-    let codeTag = document.getElementById('codeTagCard');
-    if(codeTag){
-        codeTag.innerHTML = html;
-    }
-}
-function generateGraphHTML() {
-    let html:string ="<pre>\n" +
-        "<span class='c'>&lt;!-- código HTML donde ubicar un div con un Graphic --&gt;</span>\n" +
-        "<span class='p'>&lt;</span>" +
-        "<span class='nt'>div</span> " +
-        "<span class='na'>id</span>" +
-        "<span class='o'>=</span>" +
-        "<span class='s'>'tmi'</span>" +
-        "<span class='p'>&gt;&lt;/</span> " +
-        "<span class='nt'>div</span>" +
-        "<span class='p'>&gt;</span>\n\n" +
-        "<span class='c'>&lt;!-- JS que genera el Graph en el div --&gt;</span>\n" +
-        "<span class='p'>&lt;</span>" +
-        "<span class='nt'>script</span>" +
-        "<span class='p'>&gt;</span>\n    " +
-        "<span class='nb'>window</span>" +
-        "<span class='p'>.</span>" +
-        "<span class='nx'>onload</span> " +
-        "<span class='o'>=</span> " +
-        "<span class='kd'>function</span>" +
-        "<span class='p'>()</span> " +
-        "<span class='p'>{</span>\n        " +
-        "<span class='nx'>TSComponents</span><span class='p'>.</span>" +
-        "<span class='nx'>Graphic</span>" +
-        "<span class='p'>.</span>" +
-        "<span class='nx'>render</span>" +
-        "<span class='p'>(</span>" +
-        "<span class='s1'>'tmi'</span>" +
-        "<span class='p'>,</span> "+
-        "<span class='p'>{</span>\n            " +
-        HTMLRowsForNotDefaultGraphParameters() +
-        "<span class='p'>})</span>\n    " +
-        "<span class='p'>}</span>\n" +
-        "<span class='p'>&lt;/</span>" +
-        "<span class='nt'>script</span>" +
-        "<span class='p'>&gt;</span>\n" +
-        "</pre>"
-
-    let codeTag = document.getElementById('codeTagGraph');
-    if(codeTag){
-        codeTag.innerHTML = html;
-    }
-}
 function initializeComponents() {
     if(!context) {
         context = {
@@ -394,50 +261,12 @@ function validateSeries(seriesId:Array<string>,collapse:string): Promise<AxiosRe
     console.log(paramsObject)
     return axios.get(API_SERIES_URL,paramsObject);
 }
-function updateCardErrorContainer(errorString:string){
-    let errorDiv = document.getElementById('card-error-container');
-    if(errorDiv){
-        errorDiv.textContent = errorString ;
-    }
-}function updateGraphErrorContainer(errorString:string){
-    let errorDiv = document.getElementById('graph-error-container');
-    if(errorDiv){
-        errorDiv.textContent = errorString ;
-    }
-}
-function clearCard(){
-    let errorDiv = document.getElementById('card_example');
-    if(errorDiv){
-        errorDiv.innerHTML = "" ;
-    }
-}
-function clearGraph(){
-    let errorDiv = document.getElementById('graph_example');
-    if(errorDiv){
-        errorDiv.innerHTML = "" ;
-    }
-}
-function generateChartTypeSelects(ids: Array<string>) {
-    let container = document.getElementById('chartTypeBySeries');
 
-    let baseHTMLSelect = (id:string)=> "<label for='chartTypeBySeries'>Tipo de gráfico para la serie: "+id+" </label><br>" +
-          "<select name=\"chartTypes"+id+"\" class=\"format form-control\">" +
-          "<option value=\"line\">Línea</option>" +
-          "<option value=\"area\">Área</option>" +
-          "<option value=\"column\">Columna</option>" +
-          "</select>";
-    let finalHTMLSelect ="";
-    ids.forEach((id:string)=>{
-        finalHTMLSelect+= baseHTMLSelect(id);
-    })
-    if(container){
-        container.innerHTML=finalHTMLSelect;
-    }
-}
+
 function getDefaultChartOptions(): string {
     return JSON.stringify(highChartDefaultOptions);
 }
 
 initializeComponents();
-export {initializeComponents,updateValuesCard,filterAllFalsyValues,generateCardHTML,validateSeries,updateCardErrorContainer,reRenderCardComponent,clearCard}
+export {initializeComponents,updateValuesCard,context,defaultGraphParameters,validateSeries,reRenderCardComponent}
 
